cd06ffa6dc40aae231206a4a37effcf8d8bba5c0
renaldi <renaldiiii@outlook.com>
2024-06-20 15:47:24 +0700

    footer add

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,10 +6,10 @@ export default function FooterSection() {
   return (
     <>
       <footer className="relative flex w-full mobile:w-full items-center justify-between px-4">
-        <span className="text-xs font-extarlight text-muted-foreground text-color-4 mt-16">
+        <span className="text-xs font-extarlight text-muted-foreground text-color-4 mt-16 pb-10">
           &copy;2024 Renaldi
         </span>
-        <div className="flex items-center gap-5 mt-16">
+        <div className="flex items-center gap-5 mt-16 pb-10">
           {Footer.map((image, i) => (
             <Link href="#" key={i}>
               <Image src={image.img} alt={image.alt} width={25} height={1} />
